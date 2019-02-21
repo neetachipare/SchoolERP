5bdd15a62ade730637ee9d19c94d0b0ba82c622b
nishigandhabidkar <44231294+nishigandhabidkar@users.noreply.github.com>
2019-02-21 18:23:38 +0530

    Student Admission UI Design
    Cimpleated UI design of Student admission form

---
+++ SchoolERP/Areas/SuperAdmin/Controllers/AdmissionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolERP.Areas.SuperAdmin.Controllers
+{
+    public class AdmissionController : Controller
+    {
+        // GET: SuperAdmin/Admission
+        public ActionResult Admission()
+        {
+            return View();
+        }
+    }
+}
